4f353ecec5ac326a4a8f862e3f6c87bb0a7c5ef5
rkdotdesign <rkbehera164@gmail.com>
2026-05-02 00:24:41 +0530

    migrate from Create React App to Vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,10 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+/** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-  ],
-  framework: "@storybook/react-webpack5",
-  staticDirs: ["../public"],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
+  framework: {
+    name: "@storybook/react-vite",
+  },
   env: (config) => ({
     ...config,
     PUBLIC_URL: "/propsperity",
